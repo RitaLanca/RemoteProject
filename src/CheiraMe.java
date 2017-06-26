6f73510236cb3abd8e050d32d8bd01f2fbc4b63a
RitaLanca <rita.lanca17@gmail.com>
2017-06-26 19:55:22 +0100

    Adding my first project

---
+++ src/CheiraMe.java
@@ -0,0 +1,9 @@
+
+public class CheiraMe {
+
+	public static void main(String[] args) {
+		System.out.println("Melhor aplicação de sempre");
+
+	}
+
+}
---
+++ src/Rita.java
@@ -0,0 +1,9 @@
+
+public class Rita {
+
+	public static void main(String[] args) {
+		System.out.println("Partilhem coisas");
+
+	}
+
+}
