0ad2f038938197be160c2393d665f1d98c36488f
RitaLanca <rita.lanca17@gmail.com>
2017-06-28 19:38:32 +0100

    Teste 1

--- src/CheiraMe.java
+++ src/CheiraMe.java
@@ -2,10 +2,10 @@
 public class CheiraMe {
 
 	public static void main(String[] args) {
-		int a=0;
-		System.out.println(("Melhor aplicação de sempre"));
-
 		
+		System.out.println(("Melhor aplicação de sempre"));
+		System.out.println("Teste 1");
+	
 	}
 
 }
